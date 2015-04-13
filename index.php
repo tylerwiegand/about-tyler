6b67424c92b1d748c3598f22d1a0049aacf19dd3
Tyler Wiegand <tyler.wiegand@me.com>
2015-04-13 11:36:25 -0600

    Max char length 69

--- index.php
+++ index.php
@@ -4,11 +4,14 @@
 			<title>How I Learned To Stop Worrying And Finish The Prework</title>
 	</head>
 	<body>
-		<label=text textarea maxlength="50">"At age five, Tyler had already completed the ten pre-requisites for induction into the hall of shame. By twelve
-		he had managed to wrestle an alligator, and assist with the construction of the Hoover Dam (the second one).
-		By twenty he had opened and closed ten major musical retailers, noting that the profit margins did not represent
-		the stairway to heaven pictured.
-		</label>
+		<p>
+		{
+			max-width: 100px;
+			At age five, Tyler had already completed the ten pre-requisites for induction into the hall of shame. By twelve
+			he had managed to wrestle an alligator, and assist with the construction of the Hoover Dam (the second one).
+			By twenty he had opened and closed ten major musical retailers, noting that the profit margins did not represent
+			the stairway to heaven pictured.}
+		}
 		<img src="tyler.png" alt="Tyler at 16">
 	</body>
 </html>
